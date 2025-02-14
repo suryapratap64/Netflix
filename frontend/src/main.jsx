74758a40add99778f946846ecfde7002cc4ada07
Surya Pratap <sps64044@gmail.com>
2025-02-14 18:00:19 +0530

    first

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './Index.css'
 import App from './App.jsx'
 import { Provider } from "react-redux";
 import { store } from "../src/redux/store.js";
