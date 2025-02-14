3f6baa5a17359a8232a5d67e44cb8f98cb1975a1
Surya Pratap <sps64044@gmail.com>
2025-02-14 18:56:04 +0530

    np remove

--- backends/models/user.js
+++ backends/models/user.js
@@ -1,5 +1,5 @@
 const mongoose=require('mongoose')
-mongoose.connect("mongodb://127.0.0.1:27017/movie")
+//mongoose.connect("mongodb://127.0.0.1:27017/movie")
 const userSchema=mongoose.Schema({
  
     email:String,
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,7 @@ import './Index.css'
 import App from './App.jsx'
 import { Provider } from "react-redux";
 import { store } from "../src/redux/store.js";
-np
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
      <Provider store={store}>
